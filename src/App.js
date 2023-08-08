bbcd1e21010d3b6ea62eab2b1079f66379405191
elkaholic6 <wyattchamberlin6@gmail.com>
2023-08-07 19:24:22 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import AboutMe from './Components/AboutMe';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Body from './Components/Body';
 import Navbar from './Components/Navbar';
 import Bio from './Components/Bio';
@@ -12,19 +11,6 @@ function App() {
         <div className='app_nav_container'>
           <Navbar />
         </div>
-        
-        {/* Header/navbar --> this will be quick links to each section of the portfolio */}
-        {/* Body --> My projects.  Use react components to make cards.  Will be able to scroll through screenshots of projects */}
-          {/* Discord bot */}
-            {/* 4 screenshots of different functionalities */}
-            {/* Have a link to invite bot to their server */}
-            {/* Explain that the bot saves data to mongoDB */}
-            {/* Javascript */}
-          {/* Streaming Platform */}
-            {/* 4 screenshots */}
-            {/* Link to website */}
-            {/* Showcase that it saves data to google firestore */}
-            {/* Redux-toolkit, tailwindcss, react, javascript */}
             <div className='body_container'>
               <div className='app_aboutme_wrapper'>
                 <AboutMe />
@@ -39,8 +25,6 @@ function App() {
                 <Contact />
               </div> 
             </div>
-
-        {/* Footer --> contact info and social media links */}
       </div>
   );
 }
